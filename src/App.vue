6d7a146d4789bc26d9153a11df3327219e12937b
Mustafe MC <103772721+Mustafe101@users.noreply.github.com>
2022-06-27 16:28:17 +0200

    navbar and slider (with error)

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavBar />
+  <Header />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import NavBar from './components/NavBar.vue'
+import Header from './components/HeaderOne.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
+    NavBar,
+    Header
+} 
+
+
 </script>
+
 
 <style>
 #app {
