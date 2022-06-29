4244c51fbb82ddcc001347799e65d5fb4fef4164
Mustafe MC <103772721+Mustafe101@users.noreply.github.com>
2022-06-29 11:09:55 +0200

    displayed cards

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,32 @@
 <template>
   <div id="app">
     <HeaderOne />
-    <div v-if="$route.path == '/'">
-      <!-- if the route is this one  / -->
-
       <h1 class="pt-5 font-weight-light">
         Vos films préférés sont sur Happy Movies!
       </h1>
+    
+
+
+    <div v-if="$route.path == '/'">
+      <!-- if the route is this one  / -->
+
 
       <MovieList :movies="movies" :loading="loading" :errored="errored" />
     </div>
 
     <div v-else>
       <!-- if the route is not this one  / -->
-
       <router-view v-bind:key="$route.fullPath"></router-view>
     </div>
     <FooterOne />
+
   </div>
+  <div v-bind:class="{ responseOne }"></div>
 </template>
+
+
+
+
 
 <script>
 import axios from "axios";
@@ -28,28 +36,38 @@ import MovieList from "./components/utils/MoviesList.vue";
 
 export default {
   name: "App",
+  
   components: {
     FooterOne,
     HeaderOne,
     MovieList
   },
   
-  data() {
-    return { movies: null };
+
+    el: '#app',
+    data() {
+    return {
+       movies: null,
+       loading: true,
+       errored: false
+      };
   },
 
   created() {
     axios
-      .get(
+    .get(
         "https://api.themoviedb.org/3/discover/movie?api_key=79171e57a92274a59243874f8c89c98e&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate"
       )
       .then((responseOne) => {
         this.movies = responseOne.data.results;
         console.log(this.movies);
+
       });
   },
-};
+}
+
 </script>
+
 
 
 
